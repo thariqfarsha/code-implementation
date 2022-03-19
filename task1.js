c5326f7aac941b2491ba3e6c30f8f79b6e8a291b
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-19 21:52:55 +0700

    Add comments for input validation block code

--- task1.js
+++ task1.js
@@ -1,10 +1,12 @@
 // DETEKSI PALINDROM
 
 function cekPalindrom(text) {
+  // Validasi tipe data input harus string
   if (typeof text !== "string") {
     return "Input yang dimasukkan bukan string, ubah input menjadi string!";
   }
 
+  // Validasi input harus lebih dari 2 huruf
   if (text.length <= 2) {
     return "Teks harus lebih dari 2 huruf!";
   }
