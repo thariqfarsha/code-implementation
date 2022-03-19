588ee301d984de8a36880cc841359dc1b298feeb
Thariq Farsha <mthariqfarsha@gmail.com>
2022-03-19 21:47:22 +0700

    Finish task1.js

---
+++ task1.js
@@ -0,0 +1,29 @@
+// DETEKSI PALINDROM
+
+function cekPalindrom(text) {
+  if (typeof text !== "string") {
+    return "Input yang dimasukkan bukan string, ubah input menjadi string!";
+  }
+
+  if (text.length <= 2) {
+    return "Teks harus lebih dari 2 huruf!";
+  }
+
+  let i = text.length - 1;
+  let reversed = "";
+  lowerCaseText = text.toLowerCase();
+
+  while (i >= 0) {
+    reversed = reversed + lowerCaseText[i];
+    i--;
+  }
+
+  if (lowerCaseText === reversed) {
+    return `"${text}" adalah palindrom! :D`;
+  } else {
+    return `"${text}" bukan palindrom :(`;
+  }
+}
+
+console.log(cekPalindrom("kAsuR inI ruSAk"));
+console.log(cekPalindrom("kAsuR inI baGuS"));
